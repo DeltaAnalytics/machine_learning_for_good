a9e504349ec00137e909056f33f2c781eeea3b61
Brian Spiering <bspiering@gmail.com>
2018-02-28 17:17:48 -0800

    Add spell checking to markdown cells

--- setup.sh
+++ setup.sh
@@ -12,5 +12,11 @@ conda update --all --yes
 # Get local copy of data
 git clone --single-branch --depth=1 https://github.com/DeltaAnalytics/data_science_for_good_data data
 
+# Setup spell checking
+git clone https://github.com/Calysto/notebook-extensions.git
+cd notebook-extensions
+jupyter nbextension install calysto --user
+jupyter nbextension enable calysto/spell-check/main
+
 # Start Jupyter Notebook
 jupyter notebook --browser=Chrome
