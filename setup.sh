2150b2e882fdeb1c3a0e93b3471c2ab4bd5ab5c9
Brian Spiering <bspiering@gmail.com>
2018-03-04 10:22:20 -0800

    Add logic to remove temp folder
    Hot Fix

--- setup.sh
+++ setup.sh
@@ -15,11 +15,14 @@ conda update --all --yes
 # Get local copy of data
 git clone --single-branch --depth=1 https://github.com/DeltaAnalytics/data_science_for_good_data data
 
-# Setup spell checking
+# Setup spell checking and other notebook enhancements
 git clone https://github.com/Calysto/notebook-extensions.git
 cd notebook-extensions
 jupyter nbextension install calysto --user
 jupyter nbextension enable calysto/spell-check/main
+jupyter nbextension enable calysto/cell-tools/main
+jupyter nbextension enable calysto/annotate/main
+rm -r -f notebook-extensions
 
 # Start Jupyter Notebook
 if [[ "$1" != "--no-start" ]]; then
